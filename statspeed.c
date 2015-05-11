ef476713c8d0b8e9f6fdd601960ef955ed5c3789
BMDan <github@angelfaq.com>
2015-05-11 18:04:08 +0000

    Per @DavidStrauss, stick this on the stack, instead.  Thanks!

--- statspeed.c
+++ statspeed.c
@@ -7,7 +7,7 @@
 int main(int argc, char **argv) {
     register long i;
     int rv;
-    struct stat *stat_buf;
+    struct stat stat_buf;
     long runs;
 
     if ( argc != 2 || (runs = atoi(argv[1])) <= 0 ) {
@@ -15,13 +15,8 @@ int main(int argc, char **argv) {
 	exit( 1 );
     }
 
-    if ( !( stat_buf = malloc(sizeof(struct stat)) ) ) {
-	fprintf( stderr, "Failed to alloc.\n" );
-	exit( 1 );
-    }
-
     for ( i = 0; i < runs; i++ ) {
-	if ( lstat( "/", stat_buf ) ) {
+	if ( lstat( "/", &stat_buf ) ) {
 	    fprintf( stderr, "OMGWTF - Failed lstat!?\n" );
 	    exit( 1 );
 	}

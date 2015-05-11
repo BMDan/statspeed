e2a91b7db75701185c1506f6e81c7eaffd9d5732
David Strauss <david@davidstrauss.net>
2015-05-11 11:47:48 -0700

    Whitespace fix (tabs to spaces).

--- statspeed.c
+++ statspeed.c
@@ -9,15 +9,15 @@ int main(int argc, char **argv) {
     long runs;
 
     if ( argc != 2 || (runs = atol(argv[1])) <= 0 ) {
-	    fprintf( stderr, "Usage: %s <number of runs>\n", argv[0] );
+        fprintf( stderr, "Usage: %s <number of runs>\n", argv[0] );
         exit( 1 );
     }
 
     while ( --runs > 0 ) {
-	    if ( lstat( "/", &stat_buf ) ) {
-	        fprintf( stderr, "OMGWTF - Failed lstat!?\n" );
-	        exit( 1 );
-	    }
+        if ( lstat( "/", &stat_buf ) ) {
+            fprintf( stderr, "OMGWTF - Failed lstat!?\n" );
+            exit( 1 );
+        }
     }
 
     return 0;

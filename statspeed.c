0a6a0bdad4472160ebe843f23908972363ccf546
David Strauss <david@davidstrauss.net>
2015-05-11 11:45:16 -0700

    Just count down instead of counting up and comparing.

--- statspeed.c
+++ statspeed.c
@@ -5,21 +5,20 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    register long i;
-    int rv;
     struct stat stat_buf;
     long runs;
 
-    if ( argc != 2 || (runs = atoi(argv[1])) <= 0 ) {
-	fprintf( stderr, "Usage: %s <number of runs>\n", argv[0] );
-	exit( 1 );
+    if ( argc != 2 || (runs = atol(argv[1])) <= 0 ) {
+	    fprintf( stderr, "Usage: %s <number of runs>\n", argv[0] );
+        exit( 1 );
     }
 
-    for ( i = 0; i < runs; i++ ) {
-	if ( lstat( "/", &stat_buf ) ) {
-	    fprintf( stderr, "OMGWTF - Failed lstat!?\n" );
-	    exit( 1 );
-	}
+    while ( runs > 0 ) {
+	    if ( lstat( "/", &stat_buf ) ) {
+	        fprintf( stderr, "OMGWTF - Failed lstat!?\n" );
+	        exit( 1 );
+	    }
+        --runs;
     }
 
     return 0;
